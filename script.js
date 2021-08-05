affc3c4efeafb0b4639fc6b20656cf16de726e09
Tim <tnematjonov@mail.ru>
2021-08-06 00:30:47 +0500

    end

---
+++ script.js
@@ -0,0 +1,8 @@
+const arr = [
+    { name: "Вася", age: 25 },
+    { name: "Петя", age: 30 },
+    { name: "Маша", age: 29 }
+]
+
+let result = arr.reduce((acc,item) => acc + item.age, 0) / (arr.length) ;
+console.log(result);
